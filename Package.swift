7a5f0b6f11cddd1e0365fdc2e2fc69f5b238507a
XMediator CI <vision-team@x3mads.com>
2026-09-25 20:38:37 +0000

    Release 5.6.0

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@ import PackageDescription
 let package = Package(
     name: "XMediatorLevelPlayLineAdapterCompatibility",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v13),
     ],
     products: [
         .library(name: "LevelPlayLineAdapter", targets: ["LevelPlayLineAdapterWrapper"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ironsource-mobile/LevelPlay-Line-Adapter-Swift-Package.git", exact: "5.8.0"),
+        .package(url: "https://github.com/ironsource-mobile/LevelPlay-Line-Adapter-Swift-Package.git", exact: "5.6.0"),
     ],
     targets: [
         .target(
